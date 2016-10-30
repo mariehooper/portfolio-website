c66c2175737b631413598e7bb76ea1df03a79573
Marie Hooper <marie.ashtari@gmail.com>
2016-10-30 16:27:24 -0400

    styling of hover and active nav links, removal of fading header description

--- js/scripts.js
+++ js/scripts.js
@@ -9,23 +9,12 @@ $(document).ready(function() {
       headerDescription = $('.header-description'),
       navProjects = $('.nav-projects'),
       navAbout = $('.nav-about'),
-      navContact = $('.nav-contact'),
       projects = $('#projects'),
-      about = $('#about'),
-      contact = $('#contact');
+      about = $('#about');
 
   /* Functions
      ======================================================================== */
 
-  /* GSAP Tweens
-     ======================================================================== */
-
-  /**
-   * Tween to fade header description to no opacity.
-   */
-  var tweenHeaderDescription = TweenMax.to(headerDescription, 0.5, {
-    opacity: 0
-  });
 
   /* ScrollMagic Scenes
      ======================================================================== */
@@ -33,13 +22,6 @@ $(document).ready(function() {
   /* ScrollMagic Controller to handle scenes */
   var scrollMagicController = new ScrollMagic.Controller();
 
-  /* Scene to fade header description. Lasts over duration of 20% of the height. */
-  var heroScene = new ScrollMagic.Scene({
-    triggerElement: headerDescription,
-    duration: '20%'
-  })
-  .setTween(tweenHeaderDescription)
-  .addTo(scrollMagicController);
 
   /* Waypoints
      ======================================================================== */
@@ -51,12 +33,10 @@ $(document).ready(function() {
     if (direction === 'down') {
       navProjects.addClass('active');
       navAbout.removeClass('active');
-      navContact.removeClass('active');
     }
     else {
       navProjects.removeClass('active');
       navAbout.removeClass('active');
-      navContact.removeClass('active');
     }
   },{
     offset: function() {
@@ -71,12 +51,10 @@ $(document).ready(function() {
     if (direction === 'down') {
       navProjects.removeClass('active');
       navAbout.addClass('active');
-      navContact.removeClass('active');
     }
     else {
       navProjects.addClass('active');
       navAbout.removeClass('active');
-      navContact.removeClass('active');
     }
   },{
     offset: function() {
@@ -84,23 +62,23 @@ $(document).ready(function() {
     }
   })
 
-  /**
-   * Toggles active class for Contact and removes for About
-   */
-  var contactWaypoint = contact.waypoint(function(direction) {
-    if (direction === 'down') {
-      navProjects.removeClass('active');
-      navAbout.removeClass('active');
-      navContact.addClass('active');
-    }
-    else {
-      navProjects.removeClass('active');
-      navAbout.addClass('active');
-      navContact.removeClass('active');
-    }
-  },{
-    offset: 'bottom-in-view'
-  });
+  // /**
+  //  * Toggles active class for Contact and removes for About
+  //  */
+  // var contactWaypoint = contact.waypoint(function(direction) {
+  //   if (direction === 'down') {
+  //     navProjects.removeClass('active');
+  //     navAbout.removeClass('active');
+  //     navContact.addClass('active');
+  //   }
+  //   else {
+  //     navProjects.removeClass('active');
+  //     navAbout.addClass('active');
+  //     navContact.removeClass('active');
+  //   }
+  // },{
+  //   offset: 'bottom-in-view'
+  // });
 
   /* Scripts
      ======================================================================== */
