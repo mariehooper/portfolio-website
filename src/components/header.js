eedcbfbdeea6732ea6e194621171f7c07c15eb1f
Marie Hooper <marie.ashtari@gmail.com>
2020-04-21 19:39:07 -0400

    Site redesign

--- src/components/header.js
+++ src/components/header.js
@@ -1,31 +1,44 @@
-import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
 import styles from "../styles/header.module.scss"
 
-export default function Header({ pagination }) {
+export default function Header() {
+  function handleClick(event) {
+    event.preventDefault()
+    document.querySelector(event.currentTarget.hash).scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    })
+  }
+
   return (
     <header className={styles.header}>
       <div className={styles.gridContainer}>
-        <h1 className={styles.logo}>
-          <Link to="/">Marie Hooper</Link>
-        </h1>
-        {pagination && (
-          <nav className={styles.navigation}>
-            <ul>
-              <li>
-                <Link to={pagination.previous}>
-                  Previous <span>Project</span>
-                </Link>
-              </li>
-              <li>
-                <Link to={pagination.next}>
-                  Next <span>Project</span>
-                </Link>
-              </li>
-            </ul>
-          </nav>
-        )}
+        <span className={styles.logo}>
+          <a href="#hero" onClick={handleClick}>
+            Marie Hooper
+          </a>
+        </span>
+
+        <nav className={styles.navigation}>
+          <ul>
+            <li>
+              <a href="#about" onClick={handleClick}>
+                About
+              </a>
+            </li>
+            <li>
+              <a href="#jobs" onClick={handleClick}>
+                Experience
+              </a>
+            </li>
+            <li>
+              <a href="#work" onClick={handleClick}>
+                Work
+              </a>
+            </li>
+          </ul>
+        </nav>
       </div>
     </header>
   )
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,18 @@
 import React from "react"
 import About from "../components/about"
 import Hero from "../components/hero"
+import Jobs from "../components/jobs"
+import Projects from "../components/project-grid"
 import Layout from "../components/layout"
-import ProjectGrid from "../components/project-grid"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <Hero />
-    <ProjectGrid />
     <About />
+    <Jobs />
+    <Projects />
   </Layout>
 )
 
